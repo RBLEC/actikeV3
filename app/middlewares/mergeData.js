c6374ff72b17f2dd6bc14af4762ad545b78232fc
Regis BLEC <regis.blec@gmail.com>
2024-06-09 11:09:30 +0200

    fin text header

--- app/middlewares/mergeData.js
+++ app/middlewares/mergeData.js
@@ -5,6 +5,7 @@ const servicesPath = path.join(__dirname, '../data/services.json');
 const seoConfigPath = path.join(__dirname, '../data/seoConfig.json');
 const enterprisePath = path.join(__dirname, '../data/enterprise.json');
 const ecoindexPath = path.join(__dirname, '../data/ecoindex.json');
+const siteContentPath = path.join(__dirname, '../data/siteContent.json');
 const globalDataPath = path.join(__dirname, '../data/globalData.json');
 
 const mergeData = () => {
@@ -13,12 +14,14 @@ const mergeData = () => {
         const enterpriseData = JSON.parse(fs.readFileSync(enterprisePath, 'utf8'));
         const servicesData = JSON.parse(fs.readFileSync(servicesPath, 'utf8'));
         const seoConfigData = JSON.parse(fs.readFileSync(seoConfigPath, 'utf8'));
+        const siteContentData = JSON.parse(fs.readFileSync(siteContentPath, 'utf8'));
 
         const globalData = {
             ecoindex: ecoindexData,
             enterprise: enterpriseData,
             services: servicesData.services,
-            seoConfig: seoConfigData
+            seoConfig: seoConfigData,
+            siteContent: siteContentData
         };
 
         fs.writeFileSync(globalDataPath, JSON.stringify(globalData, null, 4));
--- src/public/js/burger.js
+++ src/public/js/burger.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener('DOMContentLoaded', function() {
   var burger = document.getElementById('burger');
   var nav = document.getElementById('navbar');
@@ -5,5 +6,15 @@ document.addEventListener('DOMContentLoaded', function() {
   burger.addEventListener('click', function() {
     this.setAttribute('aria-expanded', this.getAttribute('aria-expanded') === 'false' ? 'true' : 'false');
     nav.classList.toggle('active');
+  });
+});
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
   });
 });
